7b6e897d660892c760bc8f49eb9451f1e960ab5f
forrestbthomas <forrestbthomas@gmail.com>
2014-09-22 20:26:23 -0700

    feature(client): Add test data for location tracking

--- app/views.py
+++ app/views.py
@@ -5,6 +5,17 @@ from app import app
 @app.route('/index')
 def index():
   user = { 'name' : 'Forrest' }
+  locations = [
+  {
+    'location' : '2883 Sunny Wood Cir., Santa Rosa, CA',
+    'duration' : '20 minutes'
+  },
+  {
+    'location' : '1536 48th Ave., San Francisco, CA',
+    'duration' : '20 minutes'
+  }
+  ]
   return render_template("index.html", 
     title = 'Home',
-    user = user)
+    user = user,
+    locations = locations)

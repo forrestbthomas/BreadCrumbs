e5bca57bb155eb3c759764a40489843e91bc3025
forrestbthomas <forrestbthomas@gmail.com>
2014-09-23 14:56:09 -0700

    feature(all): Add db create script and create sqlite db

---
+++ db_repository/manage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from migrate.versioning.shell import main
+
+if __name__ == '__main__':
+    main(six='<module 'six' from '/Users/forrestbthomas/Documents/python/test_project/venv/lib/python2.7/site-packages/six.pyc'>')
